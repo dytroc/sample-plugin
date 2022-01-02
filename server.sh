cf6d8338206030726a28c4fc3f560348687c72a3
dytroInc <dytrocodesworkshop@gmail.com>
2022-01-02 19:22:23 +0900

    Update Server File

--- server.sh
+++ server.sh
@@ -1,42 +1,76 @@
 #!/bin/bash
 
-server='https://papermc.io/api/v1/paper/1.17.1/latest/download'
+# 해당 스크립트는 monun님의 server-script를 사용해서 실행합니다. (https://github.com/monun/server-script)
+# 해당 서버는 공식 Paper API 말고 aroxu님의 Clip을 사용합니다. (https://github.com/aroxu/clip)
+
+### 설정 ###
+
+# 서버 마크 버전
+version='1.18.1'
+
+# 최대 사용 메모리
+memory=8
+
+# 서버 열 때 마다 config 리셋
+reload_config=false
+
+# debug 여부
+debug=true
+
+# 서버 닫힐 때, 강제로 다시 재시작할 여부
+force_restart=false
+
+# 사용할 서버 플러그인들
 plugins=(
 'https://github.com/monun/auto-reloader/releases/latest/download/AutoReloader.jar'
+'https://media.forgecdn.net/files/3559/523/worldedit-bukkit-7.2.8.jar'
 )
-script=$(basename "$0")
-server_folder=".${script%.*}"
-mkdir -p "$server_folder"
 
-server_script="start.sh"
-server_config="$server_script.conf"
+### 설정 ###
 
-if [ ! -f "$server_folder/$server_script" ]; then
-  if [ -f ".server/$server_script" ]; then
-    cp ".server/$server_script" "$server_folder/$server_script"
-  else
-    wget -qc -P "$server_folder" -N "https://raw.githubusercontent.com/monun/server-script/master/.server/$server_script"
-  fi
+
+echo "Starting Paper server..."
+
+if [ ! -d ".server" ]; then
+  echo "Creating .server directory..."
+  mkdir .server
 fi
 
-cd "$server_folder" || exit
+if [ ! -f ".server/start.sh" ]; then
+  echo "Downloading server script..."
+  wget -q -c -P ".server" -N "https://raw.githubusercontent.com/monun/server-script/master/.server/start.sh"
+fi
 
-if [ ! -f "$server_config" ]; then
-    cat << EOF > $server_config
-server=$server
-debug=true
+cd .server || return
+
+# config 파일 작성 (https://github.com/monun/server-script/blob/master/debug.sh#L26-L40)
+if [ ! -f "start.sh.conf" ] || [ $reload_config == true ]; then
+  echo "Writing config file..."
+  cat << EOF > start.sh.conf
+server="https://clip.aroxu.me/download?mc_version=$version"
+debug=$debug
 debug_port=5005
 backup=false
-force_restart=false
-memory=8
+force_restart=$force_restart
+memory=$memory
 plugins=(
 EOF
-    for plugin in "${plugins[@]}"
-    do
-        echo "  \"$plugin\""   >> $server_config
-    done
-    echo ")" >> $server_config
+  for plugin in "${plugins[@]}"
+  do
+    echo "  \"$plugin\""   >> start.sh.conf
+  done
+  echo ")" >> start.sh.conf
 fi
 
-chmod +x ./$server_script
-./$server_script launch
+if [ -f "start.sh" ]; then
+  echo "Executing server start script..."
+  chmod +x ./start.sh
+  ./start.sh launch
+
+  echo "Exiting..."
+  exit
+else
+  echo "Server script not found!"
+  echo "Exiting..."
+  exit
+fi
