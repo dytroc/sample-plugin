2aa91e087ead13d760502d9344c1fcf0d0ec0e2f
dytroInc <dytrocodesworkshop@gmail.com>
2022-01-08 22:30:26 +0900

    Kotlin 1.6.0 -> Kotlin 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
